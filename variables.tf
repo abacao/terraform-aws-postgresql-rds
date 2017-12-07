5c3951ebc0ce3b7444cbb12c56292d542ae0b236
André Bação <andre@bacao.pt>
2017-12-07 15:42:33 +0000

    removed cloudwatch stuff

--- variables.tf
+++ variables.tf
@@ -84,36 +84,36 @@ variable "parameter_group" {
   default = "default.postgres9.4"
 }
 
-variable "alarm_cpu_threshold" {
-  default = "75"
-}
-
-variable "alarm_disk_queue_threshold" {
-  default = "10"
-}
-
-variable "alarm_free_disk_threshold" {
-  # 5GB
-  default = "5000000000"
-}
-
-variable "alarm_free_memory_threshold" {
-  # 128MB
-  default = "128000000"
-}
-
-variable "alarm_cpu_credit_balance_threshold" {
-  default = "30"
-}
-
-variable "alarm_actions" {
-  type = "list"
-}
-
-variable "ok_actions" {
-  type = "list"
-}
-
-variable "insufficient_data_actions" {
-  type = "list"
-}
+# variable "alarm_cpu_threshold" {
+#   default = "75"
+# }
+#
+# variable "alarm_disk_queue_threshold" {
+#   default = "10"
+# }
+#
+# variable "alarm_free_disk_threshold" {
+#   # 5GB
+#   default = "5000000000"
+# }
+#
+# variable "alarm_free_memory_threshold" {
+#   # 128MB
+#   default = "128000000"
+# }
+#
+# variable "alarm_cpu_credit_balance_threshold" {
+#   default = "30"
+# }
+#
+# variable "alarm_actions" {
+#   type = "list"
+# }
+#
+# variable "ok_actions" {
+#   type = "list"
+# }
+#
+# variable "insufficient_data_actions" {
+#   type = "list"
+# }
